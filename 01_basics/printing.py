5433a1556c3bbabc5de4d1b7bfc434ba1f4df940
chuckablack <chuck.a.black@gmail.com>
2020-04-30 21:52:38 -0700

    printing

---
+++ 01_basics/printing.py
@@ -0,0 +1,9 @@
+from functions import create_devices
+
+devices = create_devices(25)
+
+print()
+print("        NAME      VENDOR  OS      IP ADDRESS")
+print("       -----     -------  -----   -----------")
+for device in devices:
+    print(f'    {device["name"]:>8}  {device["vendor"]:>10}  {device["os"]:<6}  {device["ip"]:<15}')
--- 01_basics/strings.py
+++ 01_basics/strings.py
@@ -26,8 +26,4 @@ for item in device1_str.split(","):
     device1.append(item.strip())
 print("device1 using strip for each item:", device1)
 
-# LIST COMPREHENSION
-device1 = [item.strip() for item in device1_str.split(",")]
-print("device1 using list comprehension:", device1)
 
-# DICT COMPREHENSION
