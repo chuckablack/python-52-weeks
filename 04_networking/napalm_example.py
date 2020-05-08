c399d82b0cef382fe0f25ee5fbb3e868c43ea5f2
chuckablack <chuck.a.black@gmail.com>
2020-05-07 21:30:33 -0700

    more napalm get_facts

--- 04_networking/napalm_example.py
+++ 04_networking/napalm_example.py
@@ -3,10 +3,15 @@ import json
 
 driver = napalm.get_network_driver('ios')
 
+print("\n----- connecting to device ----------")
 device = driver(hostname='ios-xe-mgmt-latest.cisco.com',
                 username='developer',
                 password='C1sco12345',
                 optional_args={'port': 8181})
 
 device.open()
+print("\n----- facts ----------")
+print(json.dumps(device.get_facts(), sort_keys=True, indent=4))
+
+print("\n----- interfaces ----------")
 print(json.dumps(device.get_interfaces(), sort_keys=True, indent=4))
