5cd0d4d097a73c5c3e2c27a16fcd1f2c56f340cf
chuckablack <chuck.a.black@gmail.com>
2020-05-08 19:12:52 -0700

    json and yaml output plus some napalm

--- 04_networking/napalm_example.py
+++ 04_networking/napalm_example.py
@@ -15,3 +15,9 @@ print(json.dumps(device.get_facts(), sort_keys=True, indent=4))
 
 print("\n----- interfaces ----------")
 print(json.dumps(device.get_interfaces(), sort_keys=True, indent=4))
+
+print("\n----- vlans ----------")
+print(json.dumps(device.get_vlans(), sort_keys=True, indent=4))
+
+print("\n----- snmp ----------")
+print(json.dumps(device.get_snmp_information(), sort_keys=True, indent=4))
