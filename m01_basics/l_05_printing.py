bfaf3ae5865f16f852c659170890a584b4fbafcc
chuck <chuck.a.black@gmail.com>
2020-08-01 16:21:29 -0700

    add some examples to print module

--- m01_basics/l_05_printing.py
+++ m01_basics/l_05_printing.py
@@ -1,8 +1,24 @@
 from l_03_functions import create_devices
+from pprint import pprint
+from operator import itemgetter
+from tabulate import tabulate
 
 devices = create_devices(25)
 
-print()
+print("\n\nUSING PRINT")
+print(devices)
+
+print("\n\nUSING PPRINT")
+pprint(devices)
+
+print("\n\nUSING LOOP")
+for device in devices:
+    print(device)
+
+print("\n\nUSING TABULATE")
+print(tabulate(sorted(devices, key=itemgetter("vendor", "os", "version")), headers="keys"))
+
+print("\n\nUSING LOOP AND F-STRING")
 print("        NAME      VENDOR  OS      IP ADDRESS")
 print("       -----     -------  -----   -----------")
 for device in devices:
