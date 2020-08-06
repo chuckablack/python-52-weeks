d6d8777dcc60514de81dab964c9550b3c7749dcd
chuck <chuck.a.black@gmail.com>
2020-08-06 08:36:12 -0700

    pyshark and scapy

--- m04_networking/pyshark_example.py
+++ m04_networking/pyshark_example.py
@@ -1,23 +1,33 @@
 import pyshark
 
-capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, bpf_filter='host 192.168.254.14 and not arp')
-# capture.sniff(packet_count=100)
+# CAPTURE EVERYTHING AND PRINT PACKET SUMMARIES
+print("\n----- Packet summaries --------------------")
+capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True)
+capture.sniff(packet_count=10)
+for packet in capture:
+    print(f"    {packet}")
 
-for packet in capture.sniff_continuously():
+# CAPTURE DNS AND PRINT PACKETS
+print("\n----- DNS packet summaries (collect for 50 packets) --------------------")
+capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, display_filter='dns')
+capture.sniff(packet_count=50)
+for packet in capture:
+    print(f"    {packet}")
+
+# CAPTURE ONLY DNS AND PRINT COMPLETE PACKETS
+print("\n\n----- DNS packets, complete (collect for 40 total packets) ---------------------")
+capture = pyshark.LiveCapture(interface='enp0s3', display_filter='dns')
+capture.sniff(packet_count=40)
+for packet in capture:
     print(packet)
 
-# cap = pyshark.LiveCapture(interface='enp0s3')
+# CAPTURE AND HANDLE PACKETS AS THEY ARRIVE
+print("\n\n----- Print packets as they are detected to/from specified host ---------------------")
+capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, bpf_filter='tcp port https')
 
-# def print_conversation_header(pkt):
-#     try:
-#         protocol =  pkt.transport_layer
-#         src_addr = pkt.ip.src
-#         src_port = pkt[pkt.transport_layer].srcport
-#         dst_addr = pkt.ip.dst
-#         dst_port = pkt[pkt.transport_layer].dstport
-#         print('%s  %s:%s --> %s:%s' % (protocol, src_addr, src_port, dst_addr, dst_port))
-#     except AttributeError as e:
-#         #ignore packets that aren't TCP/UDP or IPv4
-#         pass
-#
-# cap.apply_on_packets(print_conversation_header, timeout=100)
+
+def print_packet(pkt):
+    print("    ", pkt)
+
+
+capture.apply_on_packets(print_packet, packet_count=20)
--- m04_networking/scapy_example.py
+++ m04_networking/scapy_example.py
@@ -1,16 +1,25 @@
-# import scapy.all as scapy
-#
-# request = scapy.ARP()
-#
-# request.pdst = 'x'
-# broadcast = scapy.Ether()
-# broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-#
-# request_broadcast = broadcast / request
-# clients = scapy.srp(request_broadcast, timeout=1)[0]
-# for element in clients:
-#     print(element[1].psrc + "     " + element[1].hwsrc)
+import scapy.all as scapy
+
+# CAPTURE EVERYTHING AND PRINT PACKET SUMMARIES
+# print("\n----- Packet summaries --------------------")
+# capture = scapy.sniff(iface='enp0s3', count=10)
+# print(capture.nsummary())
+
+# CAPTURE DNS AND PRINT PACKETS
+print("\n----- DNS packet summaries (collect 10 DNS packets) --------------------")
+capture = scapy.sniff(iface='enp0s3', filter="udp port 53", count=10)
+print(capture.nsummary())
+
+# CAPTURE ONLY DNS AND PRINT COMPLETE PACKETS
+print("\n\n----- DNS packets, complete (collect 10 DNS packets) ---------------------")
+capture = scapy.sniff(iface='enp0s3', filter="udp port 53", count=10)
+for packet in capture:
+    print(packet.show())
 
 
-from scapy.all import *
-sniff(iface=["enp0s3"], filter='tcp port 443', prn=lambda x: x.sniffed_on + ": " + x.summary())
+# CAPTURE AND HANDLE PACKETS AS THEY ARRIVE
+def print_packet(pkt):
+    print("    ", pkt.show())
+
+
+scapy.sniff(iface='enp0s3', prn=print_packet, filter="tcp port https", count=10)
