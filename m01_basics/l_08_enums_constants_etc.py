c988248c5a20372e0eb0fcc37d80ed294cc56590
chuck <chuck.a.black@gmail.com>
2020-12-01 09:03:09 -0800

    enums adding samples

--- m01_basics/l_08_enums_constants_etc.py
+++ m01_basics/l_08_enums_constants_etc.py
@@ -4,27 +4,33 @@ from tabulate import tabulate
 from operator import itemgetter
 from enum import Enum
 
+# CONSTANTS: just use the name itself, e.g. CISCO
 # CISCO = "cisco"
 # JUNIPER = "juniper"
 # ARISTA = "arista"
 
 
-class Vendor(Enum):
-    CISCO = "cisco"
-    JUNIPER = "juniper"
-    ARISTA = "arista"
-
-
+# CLASS: use class name and class variable, e.g. Vendor.CISCO
 # class Vendor:
 #     CISCO = "cisco"
 #     JUNIPER = "juniper"
 #     ARISTA = "arista"
 
 
+# ENUM: use class name and enum, which has 'name' and 'value' attributes
+#       e.g. Vendor.CISCO.name or Vendor.CISCO.value
 # class Vendor(Enum):
 #     CISCO = 1
 #     JUNIPER = 2
 #     ARISTA = 3
+
+
+# ENUM: use class name and enum, which has 'name' and 'value' attributes
+#       e.g. Vendor.CISCO.name or Vendor.CISCO.value
+class Vendor(Enum):
+    CISCO = "cisco"
+    JUNIPER = "juniper"
+    ARISTA = "arista"
 
 
 devices = list()   # CREATE EMPTY LIST FOR HOLDING DEVICES
@@ -42,7 +48,43 @@ for index in range(20):
         + choice(string.ascii_letters)
     )
 
-    # RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER, ARISTA
+    # USING CONSTANTS: RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER, ARISTA
+    # device["vendor"] = choice([CISCO, JUNIPER, ARISTA])
+    # if device["vendor"] == CISCO:
+    #     device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
+    #     device["version"] = choice(["12.1(T).04", "14.07X", "8.12(S).010", "20.45"])
+    # elif device["vendor"] == JUNIPER:
+    #     device["os"] = "junos"
+    #     device["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
+    # elif device["vendor"] == ARISTA:
+    #     device["os"] = "eos"
+    #     device["version"] = choice(["2.45", "2.55", "2.92.145", "3.01"])
+
+    # USING CLASS: RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER, ARISTA
+    # device["vendor"] = choice([Vendor.CISCO, Vendor.JUNIPER, Vendor.ARISTA])
+    # if device["vendor"] == Vendor.CISCO:
+    #     device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
+    #     device["version"] = choice(["12.1(T).04", "14.07X", "8.12(S).010", "20.45"])
+    # elif device["vendor"] == Vendor.JUNIPER:
+    #     device["os"] = "junos"
+    #     device["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
+    # elif device["vendor"] == Vendor.ARISTA:
+    #     device["os"] = "eos"
+    #     device["version"] = choice(["2.45", "2.55", "2.92.145", "3.01"])
+
+    # USING ENUMS (NAME): RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER, ARISTA
+    # device["vendor"] = choice([Vendor.CISCO.name, Vendor.JUNIPER.name, Vendor.ARISTA.name])
+    # if device["vendor"] == Vendor.CISCO.name:
+    #     device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
+    #     device["version"] = choice(["12.1(T).04", "14.07X", "8.12(S).010", "20.45"])
+    # elif device["vendor"] == Vendor.JUNIPER.name:
+    #     device["os"] = "junos"
+    #     device["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
+    # elif device["vendor"] == Vendor.ARISTA.name:
+    #     device["os"] = "eos"
+    #     device["version"] = choice(["2.45", "2.55", "2.92.145", "3.01"])
+
+    # USING ENUMS (VALUE): RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER, ARISTA
     device["vendor"] = choice([Vendor.CISCO.value, Vendor.JUNIPER.value, Vendor.ARISTA.value])
     if device["vendor"] == Vendor.CISCO.value:
         device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
@@ -53,6 +95,7 @@ for index in range(20):
     elif device["vendor"] == Vendor.ARISTA.value:
         device["os"] = "eos"
         device["version"] = choice(["2.45", "2.55", "2.92.145", "3.01"])
+
     device["ip"] = "10.0.0." + str(index)
 
     # NICELY FORMATTED PRINT OF THIS ONE DEVICE
