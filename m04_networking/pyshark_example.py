7d1d101740d5faf48d4e86a2703d6fd6f313e017
chuck <chuck.a.black@gmail.com>
2021-03-04 09:48:45 -0800

    updating pyshark

--- m04_networking/pyshark_example.py
+++ m04_networking/pyshark_example.py
@@ -3,21 +3,21 @@ import pyshark
 # CAPTURE EVERYTHING AND PRINT PACKET SUMMARIES
 print("\n----- Packet summaries --------------------")
 capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True)
-capture.sniff(packet_count=10)
+capture.sniff(packet_count=20)
 for packet in capture:
     print(f"    {packet}")
 
 # CAPTURE DNS AND PRINT PACKETS
-print("\n----- DNS packet summaries (collect for 50 packets) --------------------")
-capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, display_filter='dns')
-capture.sniff(packet_count=50)
+print("\n----- DNS packet summaries --------------------")
+capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, bpf_filter='udp port 53')
+capture.sniff(packet_count=20)
 for packet in capture:
     print(f"    {packet}")
 
-# CAPTURE ONLY DNS AND PRINT COMPLETE PACKETS
-print("\n\n----- DNS packets, complete (collect for 40 total packets) ---------------------")
-capture = pyshark.LiveCapture(interface='enp0s3', display_filter='dns')
-capture.sniff(packet_count=40)
+# CAPTURE AND PRINT COMPLETE PACKETS
+print("\n\n----- All packets, complete ---------------------")
+capture = pyshark.LiveCapture(interface='enp0s3')
+capture.sniff(packet_count=20)
 for packet in capture:
     print(packet)
 
@@ -37,3 +37,13 @@ capture.apply_on_packets(print_packet, packet_count=20)
 print("\n\n----- Print packets as they are detected (lambda version) ---------------------")
 capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, bpf_filter='tcp port https')
 capture.apply_on_packets(lambda pkt: print("lambda    ", pkt), packet_count=20)
+
+while True:
+
+    bpf_filter = input("\n\nEnter BPF filter:")
+    if not bpf_filter:
+        break
+
+    print(f"\n----- capturing packets with BPF filter: {bpf_filter}")
+    capture = pyshark.LiveCapture(interface='enp0s3', only_summaries=True, bpf_filter=bpf_filter)
+    capture.apply_on_packets(print_packet, packet_count=20)
