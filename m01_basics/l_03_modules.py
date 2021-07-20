0e5a5e2bb3b1f141eeb082322443fc3416b4df28
chuck <chuck.a.black@gmail.com>
2021-07-20 09:00:46 -0700

    add l_03_create_devices.py

--- m01_basics/l_03_modules.py
+++ m01_basics/l_03_modules.py
@@ -1,9 +1,9 @@
 from tabulate import tabulate
+# from l_03_create_devices import create_devices
 from util.create_utils import create_devices
-
 
 # --- Main program --------------------------------------------
 if __name__ == '__main__':
 
-    devices = create_devices(num_subnets=5, num_devices=4)
+    devices = create_devices(num_subnets=5, num_devices=3, random_ip=True)
     print("\n", tabulate(devices, headers="keys"))
